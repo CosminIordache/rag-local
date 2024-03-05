89c3e4c6ee1f0cc0226fc157263cb28320ec6be2
Cosmin Iordache <cosminionutiordache@gmail.com>
2024-03-06 00:31:01 +0100

    rag done

--- data/DataFormat.py
+++ data/DataFormat.py
@@ -1,16 +1,13 @@
-from langchain_community.embeddings import LlamaCppEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def split_documents_chunks(documents):
     recursive_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=300
+        chunk_size=200,
+        chunk_overlap=50
     )
     return recursive_splitter.split_documents(documents)
 
-def embed_documents(chunks):
-    model = LlamaCppEmbeddings(model_path="/home/cosmin/Documents/models/mistral-7b-instruct-v0.1.Q5_K_M.gguf")
-    return model.embed_documents([document.page_content for document in chunks])
-
-    
-    
+def embed_documents():
+    model = OllamaEmbeddings(model="mistral")
+    return model
--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from langchain_community.vectorstores.qdrant import Qdrant
+from langchain_community.vectorstores.chroma import Chroma 
 from data.load_folder import load_folder
 from data.DataFormat import split_documents_chunks, embed_documents
+from langchain.chains import RetrievalQA
+from langchain.llms.ollama import Ollama
+
+llm = Ollama(model="mistral")
 
 folder = "/home/cosmin/VSCode/git_backend"
 documents = load_folder(folder)
 split_documents = split_documents_chunks(documents)
-embeddings = embed_documents(split_documents)
+embeddings = embed_documents()
 
-print(split_documents)
-print("-------------------------------------------------------------------------------")
-print(embeddings)
+# Load the Chroma database from disk
+chroma_db = Chroma(persist_directory="data-db", 
+                   embedding_function=embeddings,
+                   collection_name="test")
+
+# Get the collection from the Chroma database
+collection = chroma_db.get()
+
+# If the collection is empty, create a new one
+if len(collection['ids']) == 0:
+    # Create a new Chroma database from the documents
+    chroma_db = Chroma.from_documents(
+        documents=split_documents, 
+        embedding=embeddings, 
+        persist_directory="data-db",
+        collection_name="test"
+    )
+
+    # Save the Chroma database to disk
+    chroma_db.persist()
+
+
+rag = RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever= chroma_db.as_retriever(),
+    return_source_documents=True,
+    verbose=True
+)
+
+result = rag.invoke("what do the routes.py file?")
+print(result['result'])
